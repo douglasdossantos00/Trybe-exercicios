b488667b34c7845426488edd78483bc248b0cd09
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-05 20:26:36 -0300

    exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/primeirosPassos.js
@@ -0,0 +1,11 @@
+const a = 6;
+const b = 7;
+
+console.log("Adição = ", a + b);
+console.log("Subtração= ", a - b);
+console.log("multiplicação= ", a * b);
+console.log("Divisão= ", a / b);
+console.log("Módulo = ", a % b);
+
+
+
