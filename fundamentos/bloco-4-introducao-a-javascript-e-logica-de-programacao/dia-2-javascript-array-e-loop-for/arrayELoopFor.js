432141695a85cfe532a3c8bbbc0c06096068acb6
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-08 18:54:23 -0300

    exercicio 3, média aritimética e impressão

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
@@ -10,10 +10,23 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // *exercicio 2, somando todos valores e imprimindo
 
-let resultado = 0;
+/* let resultado = 0;
 for(let i=0; i < numbers.length; i++) {
-    resultado *= numbers[i];
+    resultado += numbers[i];
 } 
 console.log("resultado da soma =", resultado);
+ */
+
+// * exercicio 3, média aritimética e impressão
+
+numeroDeitens = numbers.length;
+resultado = 0;
+for(let i=0; i < numbers.length; i++) {
+    resultado += numbers[i]
+}
+console.log(" Média aritimética = ", (resultado/numeroDeitens));
+
+
+
 
 
