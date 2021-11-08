d4a81fdd6cb33d21e558ef0dbe646f3204621c6c
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-08 19:16:20 -0300

    exercicio 4, verificando se é maior/menor que 20 e imprimindo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
@@ -2,7 +2,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // * exercicio 1, percorrendo o array e imprimindo
-
  
 /* for(let i=0; i < numbers.length; i++) {
     console.log(numbers [i])
@@ -10,21 +9,42 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // *exercicio 2, somando todos valores e imprimindo
 
-/* let resultado = 0;
+ /* let resultado = 0;
 for(let i=0; i < numbers.length; i++) {
-    resultado += numbers[i];
+    resultado = resultado + numbers[i];
 } 
 console.log("resultado da soma =", resultado);
  */
 
 // * exercicio 3, média aritimética e impressão
 
-numeroDeitens = numbers.length;
+/* numeroDeitens = numbers.length;
 resultado = 0;
 for(let i=0; i < numbers.length; i++) {
     resultado += numbers[i]
 }
 console.log(" Média aritimética = ", (resultado/numeroDeitens));
+ */
+
+// *exercicio 4, verificando se é maior/menor que 20 e imprimindo
+
+ numeroDeitens = numbers.length;
+resultado = 0;
+
+for(let i=0; i < numbers.length; i++) {
+    resultado += numbers[i];
+}
+let mediaAritimetica = (resultado/numeroDeitens)
+if(mediaAritimetica > 20){
+    console.log("Valor maior que 20");
+}    
+    else{
+        console.log("Valor menor que 20")
+    }
+
+
+
+/*console.log(" Média aritimética = ", (resultado/numeroDeitens)); */
 
 
 
