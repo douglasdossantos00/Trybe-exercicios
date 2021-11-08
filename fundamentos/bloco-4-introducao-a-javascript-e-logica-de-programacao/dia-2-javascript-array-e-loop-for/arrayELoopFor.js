41947218cea9ee9b07c91745ed2bff88eb545860
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-08 18:42:24 -0300

    exercicio 2, somando todos valores e imprimindo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
@@ -1,6 +1,19 @@
-// * exercicio 1, percorrendo o array e imprimindo
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for(let i=0; i < numbers.length; i++) {
+
+// * exercicio 1, percorrendo o array e imprimindo
+
+ 
+/* for(let i=0; i < numbers.length; i++) {
     console.log(numbers [i])
-}
+} */
+
+// *exercicio 2, somando todos valores e imprimindo
+
+let resultado = 0;
+for(let i=0; i < numbers.length; i++) {
+    resultado *= numbers[i];
+} 
+console.log("resultado da soma =", resultado);
+
+
