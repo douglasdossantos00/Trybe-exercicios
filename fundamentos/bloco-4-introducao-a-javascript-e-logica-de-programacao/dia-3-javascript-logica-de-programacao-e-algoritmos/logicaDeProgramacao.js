369cbc32927bbebf1f5e8988b7b93c1125ed20a1
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-09 18:29:33 -0300

    Exercicio 1 imprimir um quadrado de asterisco conforme valor n

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
@@ -0,0 +1,12 @@
+// *Exercicio 1 imprimir um quadrado de asterisco conforme valor n
+
+let number = 10;
+let nAsterisk = "*";
+let nLine = ""; 
+for (i=0; i<number; i++){
+    nLine += nAsterisk;
+}
+for(i=0; i<number; i++) {
+    console.log(nLine)
+}
+
