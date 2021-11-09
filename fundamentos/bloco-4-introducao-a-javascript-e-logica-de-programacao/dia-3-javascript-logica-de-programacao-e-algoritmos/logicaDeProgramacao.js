b492daa54dc3ed69f8d79d7bfe03ca44333fc8a8
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-09 18:50:00 -0300

    Exercicio 2, imprima um triângulo retângulo com n asteriscos de base

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
@@ -1,12 +1,21 @@
-// *Exercicio 1 imprimir um quadrado de asterisco conforme valor n
+// *Exercicio 1, imprimir um quadrado de asterisco conforme valor n
 
-let number = 10;
-let nAsterisk = "*";
-let nLine = ""; 
+/*  
 for (i=0; i<number; i++){
     nLine += nAsterisk;
 }
 for(i=0; i<number; i++) {
     console.log(nLine)
+} */
+
+
+// *Exercicio 2, imprima um triângulo retângulo com n asteriscos de base.
+
+const number = 5;
+let nAsterisk = "*";
+let nLine = "";
+for (i=0; i<number; i++) {
+    nLine += nAsterisk;
+    console.log(nLine);
 }
 
