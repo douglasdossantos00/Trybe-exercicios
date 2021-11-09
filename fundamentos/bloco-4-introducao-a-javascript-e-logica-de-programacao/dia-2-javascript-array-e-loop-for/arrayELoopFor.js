50be6474c52737cd134edd1fc360b8d0f0d61375
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-09 15:32:56 -0300

    salvando os exercicios do dia

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
@@ -0,0 +1,112 @@
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// *Exercicio 1, percorrendo o array e imprimindo
+
+/* for(let i=0; i < numbers.length; i++) {
+    console.log(numbers [i])
+} */
+
+
+// *Exercicio 2, somando todos valores e imprimindo
+
+ /* let resultado = 0;
+for(let i=0; i < numbers.length; i++) {
+    resultado = resultado + numbers[i];
+} 
+console.log("resultado da soma =", resultado);
+ */
+
+
+// * Exercicio 3, média aritimética e impressão
+
+/* numeroDeitens = numbers.length;
+resultado = 0;
+for(let i=0; i < numbers.length; i++) {
+    resultado += numbers[i]
+}
+console.log(" Média aritimética = ", (resultado/numeroDeitens));
+ */
+
+
+// *Exercicio 4, verificando se é maior/menor que 20 e imprimindo
+
+/*  numeroDeitens = numbers.length;
+resultado = 0;
+
+for(let i=0; i < numbers.length; i++) {
+    resultado += numbers[i];
+}
+let mediaAritimetica = (resultado/numeroDeitens)
+if(mediaAritimetica > 20){
+    console.log("Valor maior que 20");
+}    
+    else{
+        console.log("Valor menor que 20")
+    }
+ */
+
+
+// *Exercicio 5, descobrindo maior valor do array com for
+
+/* let numero = 0;
+for(let i=0; i < numbers.length; i++){
+    if(numbers[i] > numero){
+        numero=numbers[i]
+    }
+}
+console.log("maior valor do array ="numero); */
+
+
+// *Exercicio 6, descobrindo valores impares
+
+/* let numerosImpares = [];
+for(let i=0; i < numbers.length; i++){
+    if ((numbers[i] % 2) !== 0){
+    numerosImpares.push(numbers[i]); 
+    }
+}
+console.log(numerosImpares);
+if(numerosImpares.length == 0){
+    console.log("nenhum valor ímpar encontrado");
+} */
+
+
+// *Exercicios 7, menor valor com for
+
+/* let numero = numbers[1];
+for(let i=0; i < numbers.length; i++){
+    if(numbers[i] < numero){
+        numero=numbers[i]
+    }
+}
+console.log("menor valor do array =", numero);
+ */
+
+
+// *Exercicio 8, criando array de 1 a 25
+
+ let novoArray = [];
+for(let i=1; i < 26; i++){
+    novoArray.push(i);
+}
+console.log(novoArray); 
+
+
+// *Exercicio 9, imprimindo a divisão dos elementos do array anterior por 2
+
+for (let i = 0; i < novoArray.length; i ++){
+    console.log(novoArray[i]/2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
