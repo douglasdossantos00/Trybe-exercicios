bd5b9339285fc5276fcb9679e84412545aecdb44
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-11 20:28:44 -0300

    exercicio 2, incluir nova propriedade no objeto e impimir o objeto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetosEFuncoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetosEFuncoes.js
@@ -1,14 +1,22 @@
 /* 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, 
     incluindo seu nome. Valor esperado no console: */
 //criar objeto
-let info = {
+ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
 // imprimir boas vindas para nome da personagem
-    console.log("Seja bem vinda ", info.personagem)
+    //console.log("Seja bem vinda ", info.personagem)
+ 
+
+/* 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e,
+ em seguida, imprima o objeto no console. Valor esperado no console:    
+ */
+info["recorrente"]="Sim";
+console.log(info);
+
 
 
 
