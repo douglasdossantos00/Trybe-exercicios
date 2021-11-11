97920115bf68a349612eb71a760afbe5fa9e8994
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-11 18:48:34 -0300

    exercicio 1, imprimindo boas vindas ao personagem

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetosEFuncoes.js
@@ -0,0 +1,18 @@
+/* 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, 
+    incluindo seu nome. Valor esperado no console: */
+//criar objeto
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+// imprimir boas vindas para nome da personagem
+    console.log("Seja bem vinda ", info.personagem)
+    
+
+
+
+
+  
+
