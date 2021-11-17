82341e54e11306cf21e9a455e109a804262949ba
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-17 14:28:17 -0300

    exercicio administrador de tempo

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/domESeletores.js
@@ -0,0 +1,12 @@
+/* //exercicio administrador de tempo
+document.getElementsByTagName('h1')[0].style.background= "rgb(0, 176, 105)";
+document.querySelector(".emergency-tasks").style.background = "rgb(255, 159, 132)";
+document.querySelector(".no-emergency-tasks").style.background = "rgb(249, 219, 94)"
+document.querySelector("#footer-container").style.background = "rgb(0, 53, 51)"
+const h3Emergency = document.querySelectorAll(".emergency-tasks h3");
+for (i = 0; i < h3Emergency.lenght; i += 1){
+    h3Emergency[i].style.background = "rgb(165, 0, 243)";
+   
+} */
+
+//exercicio funções de manipulação do DOM
