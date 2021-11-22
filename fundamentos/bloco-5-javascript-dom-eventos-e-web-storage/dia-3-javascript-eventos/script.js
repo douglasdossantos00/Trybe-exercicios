db13b39d7fa389a796ca3fe695f7ec5fd4ce7cf3
douglasdossantos00 <dougladossantos00@gmail.com>
2021-11-22 09:40:48 -0300

    exercicio 5

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -80,4 +80,46 @@ function changeColor(){
 }
 changeColor()
 
+/* Exercício 4:
+Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+Adicione a este botão o ID "btn-friday" .
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+ */
+
+function buttonFriday (name) {
+  let buttonContainer = document.querySelector(".buttons-container");
+  let button = document.createElement('button');
+  button.innerText = name;
+  button.id = "btn-friday"
+  button.className = "buttons-container"
+  buttonContainer.appendChild(button)
+}
+buttonFriday("Sexta-feira")
+
+/* Exercício 5:
+Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+ */
+function clickFriday(array){ 
+  let buttonFriday = document.querySelectorAll(".friday");
+  let newTxt = "Sextou BB";
+  
+  document.querySelector("#btn-friday").addEventListener("click", function() {
+    for (let i = 0; i < buttonFriday.length; i += 1){
+      if (buttonFriday[i].innerText !== newTxt){
+        buttonFriday[i].innerText = newTxt;
+      } else{
+        buttonFriday[i].innerText = array[i];
+      }
+    }
+  })
+}
+clickFriday([4, 11, 18, 25])
+
+/* Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, 
+  o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
+ */
+
+
+
 
